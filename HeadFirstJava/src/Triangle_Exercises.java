68a0e82f12fe240ba7feb32feeb86fc583e3af33
tylor.borgeson@gmail.com <tylor.borgeson@gmail.com>
2017-02-14 09:57:28 +0100

    add triangle exercises

--- HeadFirstJava/src/Triangle_Exercises.java
+++ HeadFirstJava/src/Triangle_Exercises.java
@@ -1,5 +1,6 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 /**
  * Created by tborgeso on 08/02/2017.
  */
